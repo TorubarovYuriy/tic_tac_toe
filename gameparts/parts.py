05bdcb2bce7f1a71758d1daa5104eff6822bf065
Юрий Торубаров <yurec1412@yandex.ru>
2024-02-06 21:12:48 +0300

    Add new function 'save_result' which saves the result of the game to a file

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -7,7 +7,7 @@ class Board:
         self.board = [
             [' ' for _ in range(self._field_size)]
             for _ in range(self._field_size)
-              ]
+        ]
 
     def __str__(self) -> str:
         return (
@@ -19,7 +19,7 @@ class Board:
         """Метод, который обрабатывает ходы игроков."""
         self.board[row][col] = player
 
-    def display(self):
+    def display(self) -> None:
         """Метод, который отрисовывает игровое поле."""
         for row in self.board:
             print('|'.join(row))
@@ -32,22 +32,26 @@ class Board:
                 if self.board[i][j] == ' ':
                     return False
         return True
-    
-    def check_win(self, player):
+
+    def check_win(self, player) -> bool:
         """Метод который определяет победу"""
         for i in range(self._field_size):
             if (all([self.board[i][j]] == player
                     for j in range(self._field_size)) or
-                all([self.board[i][j] == player
-                     for j in range(self._field_size)])):
+                    all([self.board[i][j] == player
+                         for j in range(self._field_size)])):
                 return True
             if (
-                self.board[0][0] == self.board[1][1] == self.board[2][2]
-                == player
-                or
-                self.board[0][2] == self.board[1][1] == self.board[2][0]
-                == player
+                    self.board[0][0] == self.board[1][1] == self.board[2][2]
+                    == player
+                    or
+                    self.board[0][2] == self.board[1][1] == self.board[2][0]
+                    == player
             ):
                 return True
         return False
-                
+
+    def save_result(self, result: str) -> None:
+        """Метод который добавляет результат игры в файл"""
+        with open('results.txt', 'a', encoding='utf-8') as f:
+            f.write(result + '\n')
